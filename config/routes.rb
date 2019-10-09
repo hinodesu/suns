7fde887af20d69ebc5873da398e71b6b3b24acd2
hino <3427@gmail.com>
2019-10-09 14:52:50 +0900

    Top画面と行事予定画面の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/top'
+
+  resources :annual_events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
