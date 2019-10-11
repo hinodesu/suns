813e67b1eb59f3c2537238a28396bf80d73caf33
古川晃太 <furukou1127@example.com>
2019-10-11 14:52:15 +0900

    提出物一覧の検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
     get 'home/top'
-    
+
+    post '/submissions_search', to: 'submissions#search'
+    post '/', to: 'submissions#search'
+
     resources :submissions
     resources :annual_events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
