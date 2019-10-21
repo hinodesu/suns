8cfabed111118b2cfccec274a2eae7bb1a337432
3434 <3434@example.com>
2019-10-21 11:29:58 +0900

    ユーザー画面のボタンの追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,12 @@ Rails.application.routes.draw do
 
     get 'home/top'
 
-<<<<<<< HEAD
     post'/users_search', to: 'users#search'
     post'/', to: 'user#search'
-=======
     post '/submissions_search', to: 'submissions#search'
     post '/', to: 'submissions#search'
->>>>>>> c1ebb7533d573623beea256df1ca7bf22a28f807
 
+    
     resources :submissions
     resources :annual_events
     resources :users
