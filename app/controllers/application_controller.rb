abaa54c9f118024d98e6666b9b96ea2861dd42de
hino <3427@gmail.com>
2019-11-12 15:15:16 +0900

    ログインしてなかったら

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
+  def login_check
+        if session[:user_id].blank?
+          redirect_to home_login_path, notice: "ログインされてないか、セッションが切れています。再度ログインしてください。", layout: nil
+        end
+   end
   protect_from_forgery with: :exception
 end
