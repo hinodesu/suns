a198eea99035a766e41a61c852ac597dbc8e06e8
3434 <3434@example.com>
2019-12-08 22:12:43 +0900

    バリデーション"

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,9 @@ class User < ApplicationRecord
 
     def error_check
         #numberが空の時にエラーメッセージを追加する
-        #if number.blank?
-        #   errors[:base] << '学籍番号を入力してください'
-        #end
+        if number.blank?
+           errors[:base] << '学籍番号は半角で入力してください'
+        end
 
         #nameが空の時にエラーメッセージを追加する
         # if name.blank?
